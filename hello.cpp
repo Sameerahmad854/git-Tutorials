37145eff3cd53b8b6154269e04b29b3014ad142e
Sameerahmad854 <codewithsameer4@gmail.com>
2025-11-17 18:30:18 -0800

    cout << pakistan << endl;

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,6 @@ int main()
           cout << "hello git" << endl;
           cout << "hello g" << endl;
           cout << "what are you doing" << endl;
+          cout << "pakistan" << endl;
           return 0;
 }
